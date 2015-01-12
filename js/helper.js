2b3d6aba15b2bf9fb21c17bdce9b38de269584f5
randy rees <randyrrees@gmail.com>
2015-01-11 21:33:07 -0500

    Add my files to original repo

--- js/helper.js
+++ js/helper.js
@@ -43,17 +43,18 @@ var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLcertClasses = '<h3 class="justify">Certifications</h3>';
+var HTMLcertInstitute = '<a href="#">%data%';
+var HTMLcertTitle = ' - %data%</a>';
+var HTMLcertDate = '<div class="date-text">%data%</div>';
+var HTMLcertURL = '<br><a href="#">%data%</a>';
+var HTMLcertID = '<em><br>Career ID: %data%</em>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -85,7 +86,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);/// your code goes here!
 });
 
 
@@ -124,7 +127,7 @@ function initializeMap() {
     var locations = [];
 
     // adds the single location property from bio to the locations array
-    locations.push(bio.contacts.location);
+    locations.push(bio.contactinfo.location);
 
     // iterates through school locations and appends each location to
     // the locations array
@@ -171,6 +174,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -232,12 +236,12 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,256 @@
+var name = "Randy Rees";
+var formattedName = HTMLheaderName.replace("%data%",name);
+var role = "The Man";
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+var bio = {
+	"name": "Randy Rees",
+	"role": "Capacity Planning Manager/Web Developer",
+	"contactinfo": {
+		"mobile": "440-555-5555",
+		"email": "randy@r33s.com",
+		"github": "r33s",
+		"twitter": "@drfilips",
+		"location": "Cleveland"
+	},
+	"bioPic": "images/mechillin.jpg",
+	"welcomeMessage" : "Greetings new employer!",
+	"skills": [
+	"project management", "programming", "web development", "networking"
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+		"employer": "Ameritech/SBC/at&t",
+		"title": "technician",
+		"dates": "June 2000 - February 2012",
+		"location": "Cleveland, OH",
+		"description": "Performed multiple jobs as a technician for Ameritech, SBC, & AT&T. Construction Lineman, Copper/Fiber Splicer, Facilities specialist in the MLAC, Cable locator, Cable Repairman, and DSL installer."
+		},
+		{
+		"employer": "at&t",
+		"title": "Capacity Planning Manager",
+		"dates": " February 2012 - present",
+		"location": "Cleveland, OH",
+		"description": "Project manager of several large scale initiatives including FTB, ASE, and FTTP. Responsible for the facilities within 10 wire centers in the Cleveland area. This includes equipment, fiber, and copper cables."
+		},
+		{
+		"employer": "Procare Vision Center",
+		"title": "Owner/Web Master/Network Administrator",
+		"dates": "November 2009 - present",
+		"location": "Berea, OH",
+		"description": "Help my wife run a successful optometry practice. Responsible for the web site and all IT issues including securiy and hardware. Also involved in all personnel desicions."
+		}
+	]
+	
+};
+
+var education = {
+	"schools": [
+		{
+			"name": "Cleveland State University",
+			"location": "Cleveland, OH",
+			"degree": "MS",
+			"majors": ["Electrical Engineering - Focus in Computer Engineering"],
+			"date": 2013,
+			"url": "http://www.csuohio.edu/engineering"
+		},
+		{
+			"name": "Cleveland State University",
+			"location": "Cleveland, OH",
+			"degree": "BS",
+			"majors": ["Electrical Engineering Technology"],
+			"date": 2012 + " - Summa Cum Laude",
+			"url": "http://www.csuohio.edu/engineering"
+		},
+		{
+			"name": "Udacity",
+			"location": "Olmsted Falls, OH",
+			"degree": "Nanodegree",
+			"majors": "Front-End Web Developer",
+			"date": "In progress - expected late Spring 2015",
+			"url": "https://www.udacity.com/course/nd001"
+		}
+	],
+	"certs": [
+		{
+			"title": "Cisco Certified Network Associate - CCNA",
+			"institute": "Cisco",
+			"id": "CSCO12473485",
+			"date": "Expires: September 19, 2016",
+			"image": "images/mechillin.jpg",
+		},
+		{
+			"title": "Security+",
+			"institute": "CompTIA",
+			"id": "COMP001020305215",
+			"date": "Expires: May 23, 2016",
+			"image": "images/mechillin.jpg",
+		},
+		{
+			"title": "Network+",
+			"institute": "CompTIA",
+			"id": "COMP001020305215",
+			"date": "Expires: May 23, 2016",
+			"image": "images/mechillin.jpg",
+		},
+		{
+			"title": "A+",
+			"institute": "CompTIA",
+			"id": "COMP001020305215",
+			"date": "Expires: May 23, 2016",
+			"image": "images/mechillin.jpg",
+		},
+		{
+			"title": "Six Sigma Green Belt",
+			"institute": "at&t",
+			"id": "N/A",
+			"date": "Certified November 15, 2013",
+			"image": "images/mechillin.jpg",
+		}
+	]
+	
+};
+
+var projects = {
+	"projects": [
+		{
+		"title": "Interactive Resume",
+		"dates": 2014,
+		"description": "Create an interactive website using JavaScript.",
+		"images": [""]
+		},
+		{
+		"title": "Mockup to Website",
+		"dates": 2014,
+		"description": "Duplicate a website design and layout based of a PDF document.",
+		"images": [""]
+		}
+	]
+};
+
+var formattedMobile = HTMLmobile.replace("%data%",bio.contactinfo.mobile);
+var formattedEmail = HTMLemail.replace("%data%",bio.contactinfo.email);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contactinfo.github);
+var formattedTwitter = HTMLtwitter.replace("%data%",bio.contactinfo.twitter);
+var formattedLocation = HTMLlocation.replace("%data%",bio.contactinfo.location);
+var formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPic);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedTwitter);
+$("#topContacts").append(formattedLocation);
+$("#footerContacts").append(formattedMobile);
+$("#footerContacts").append(formattedEmail);
+$("#footerContacts").append(formattedGithub);
+$("#footerContacts").append(formattedTwitter);
+$("#footerContacts").append(formattedLocation);
+
+
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+console.log(formattedMobile);
+
+if(bio.skills != 0){
+	$("#header").append(HTMLskillsStart);
+	for(var i=0; i!=bio.skills.length; i++){
+		var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
+		$("#skills").append(formattedSkills);
+	}
+}
+
+function displayWork(){
+	for(job in work.jobs){
+		$("#workExperience").append(HTMLworkStart);
+		var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedWorkEmployer + formattedWorkTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+		var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+		var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedWorkLocation);
+		$(".work-entry:last").append(formattedWorkDates);
+		$(".work-entry:last").append(formattedWorkDescription);
+	}
+}
+
+function displayEducation(){
+	for(school in education.schools){
+		$("#education").append(HTMLschoolStart);
+		var formattedSchoolName = HTMLschoolName.replace("#", education.schools[school].url).replace("%data%", education.schools[school].name);
+		var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedSchoolInfo = formattedSchoolName + formattedSchoolDegree;
+		$(".education-entry:last").append(formattedSchoolInfo);
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].date);
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+		//var formattedSchoolURL = HTMLschoolURL.replace("%data%", education.schools[school].url);
+		$(".education-entry:last").append(formattedSchoolLocation);
+		$(".education-entry:last").append(formattedSchoolDates);
+		//$(".education-entry:last").append(formattedSchoolURL);
+		$(".education-entry:last").append(formattedSchoolMajor);
+		}
+
+	if(education.certs != 0){
+		$(".education-entry:last").append(HTMLcertClasses);
+		for(cert in education.certs){
+			var formattedCertTitle = HTMLcertTitle.replace("%data%",education.certs[cert].title);
+			var formattedCertInstitute = HTMLcertInstitute.replace("%data%",education.certs[cert].institute);
+			var formattedInstituteTitle = formattedCertInstitute + formattedCertTitle;
+			$(".education-entry:last").append(formattedInstituteTitle);
+			var formattedCertDate = HTMLcertDate.replace("%data%", education.certs[cert].date);
+			//var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+			var formattedCertID = HTMLcertID.replace("%data%", education.certs[cert].id);
+			$(".education-entry:last").append(formattedCertDate);
+			$(".education-entry:last").append(formattedCertID);
+		}
+	}
+}
+
+displayWork();
+displayEducation();
+
+projects.display = function() {
+	for(project in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		//var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+		$(".project-entry:last").append(formattedProjectTitle);
+		$(".project-entry:last").append(formattedProjectDates);
+		$(".project-entry:last").append(formattedProjectDescription);
+		//$(".project-entry:last").append(formattedProjectImages);
+	}
+}
+
+projects.display();
+
+inName(name);
+
+function inName(headerName) {
+    var internationalName = headerName;
+    var space = ' ';
+    var names = internationalName.split(space);
+    var lastName = names.pop();
+    lastName = lastName.toUpperCase();
+    var firstName = names.pop();
+    firstName = firstName.toLowerCase();
+    var lengthFname = firstName.length;
+    var unit = 1-lengthFname;
+    var endFname = firstName.slice(1);
+    var firstLetter = firstName.slice(0,unit);
+    firstLetter = firstLetter.toUpperCase();
+    internationalName = firstLetter + endFname + ' ' + lastName;
+    return internationalName;
+}
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
